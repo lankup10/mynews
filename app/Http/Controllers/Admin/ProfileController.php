80710bb2758b4852f2f53700a28e843be3e7eb89
”Tatsuya” <lankup10@yahoo.co.jp>
2020-10-10 02:15:59 +0000

    1010-2nd

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -28,3 +28,11 @@ class ProfileController extends Controller
         return redirect('admin/profile/edit');
     }
 }
+
+
+// 1.ControllerとRoutingについてわからないこと
+// →そもそも詳細な説明への言及がないので、今のところなし
+// 2.Controllerの役割
+// →Routingからデータを受け取り、ModelやViewとやりとりをして、データを取得・保存したり、表示データを作成したりする。
+// 3.ControllerとRoutingの役割
+// →Routingはユーザーからアクセスされたデータを受け取り、そのデータをControllerの中に作成されたActionに渡す。その後2.で記載したとおり。
