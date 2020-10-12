33b1d9ec0c8bfc42d9e4d165bec1a9e7a1318a66
”Tatsuya” <lankup10@yahoo.co.jp>
2020-10-12 15:14:45 +0000

    1013-commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+
+// 以下、「PHP/Laravel 09 Routing」課題
+// 課題１：Routing
+
+// 課題２：同じURLを含む複数のページ（例：課題４のadmin/profile/createやadmin/profile/edit）に対して、
+//         まとめて設定をすることができるという点。
+
+// 課題３：以下のとおり回答します。
+// Route::get('XXX','AAAController@bbb')
