cc74b09c108533543acc719364a44526aaa81371
”Tatsuya” <lankup10@yahoo.co.jp>
2020-10-14 13:44:21 +0000

    1014-1st

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>PrfileEdit</title>
+</head>
+<body>
+    <h1>Profile編集画面</h1>
+</body>
+</html>
