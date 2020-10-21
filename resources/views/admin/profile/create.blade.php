3a64d2e5d205b4332ff120d9016fe5792c84b607
”Tatsuya” <lankup10@yahoo.co.jp>
2020-10-21 23:15:35 +0000

    1022-1st

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,27 +1,40 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'My プロフィール'を埋め込む --}}
+@section('title', 'My プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+<!--<!DOCTYPE html>-->
+<!--<html lang="ja">-->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1">-->
     
-    <title>ProfileCreate</title>
-</head>
-<body>
-    <h1>Profile作成画面</h1>
-</body>
-</html>
+<!--    <title>ProfileCreate</title>-->
+<!--</head>-->
+<!--<body>-->
+<!--    <h1>Profile作成画面</h1>-->
+<!--</body>-->
+<!--</html>-->
 
-// 以下、「PHP/Laravel 10 ControllerとViewが連携できるようにしよう」課題
-// 課題１
-// →　Controllerからの指示を受けて、ユーザーに表示する画面を作成するところ。
-// 課題２
-// →　ユーザー名など、そのユーザーに応じたHTMLデータを表示するため。
-　　　プログラミング言語やフレームワークを使わないと、固定されたHTMLデータしか表示できない。
-// 課題３
-// →　add Action:　admin/profile配下にcreate.blade.phpというファイルを設置
-// →　edit Action:　admin/profile配下にedit.blade.phpというファイルを設置
-　　　
-
-
-
+<!--// 以下、「PHP/Laravel 10 ControllerとViewが連携できるようにしよう」課題-->
+<!--// 課題１-->
+<!--// →　Controllerからの指示を受けて、ユーザーに表示する画面を作成するところ。-->
+<!--// 課題２-->
+<!--// →　ユーザー名など、そのユーザーに応じたHTMLデータを表示するため。-->
+<!--　　　プログラミング言語やフレームワークを使わないと、固定されたHTMLデータしか表示できない。-->
+<!--// 課題３-->
+<!--// →　add Action:　admin/profile配下にcreate.blade.phpというファイルを設置-->
+<!--// →　edit Action:　admin/profile配下にedit.blade.phpというファイルを設置-->
